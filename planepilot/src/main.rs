2f1163e9f499027ba8795ff809fc85da070dc34b
Scott <scott.brugmans@gmail.com>
2024-06-16 20:48:15 +0200

    added first TECS

--- planepilot/src/main.rs
+++ planepilot/src/main.rs
@@ -12,12 +12,18 @@ pub struct AppState {
     flying: bool,
     plane_state: HashMap<String, Value>,
     horizontal_mode: HorizontalModes,
+    vertical_mode: VerticalModes,
 }
 
 enum HorizontalModes {
     Standby,
     WingsLevel,
     Heading,
+}
+
+enum VerticalModes {
+    Standby,
+    TECS,
 }
 
 #[derive(Debug)]
@@ -59,7 +65,8 @@ async fn run_autopilot() -> anyhow::Result<()> {
     let mut app_state = AppState {
         flying: false,
         plane_state: HashMap::new(),
-        horizontal_mode: HorizontalModes::Heading,
+        horizontal_mode: HorizontalModes::WingsLevel,
+        vertical_mode: VerticalModes::TECS,
     };
 
     loop {
@@ -95,6 +102,113 @@ async fn run_autopilot() -> anyhow::Result<()> {
         };
 
         if app_state.flying {
+            match app_state.vertical_mode {
+                VerticalModes::Standby => {}
+                VerticalModes::TECS => {
+                    let target_altitude: f64 = 3000.0; // 3000 ft
+                    let target_speed = 100.0; // 100 kts
+
+                    let target_kinetic: f64 =
+                        0.5 * (target_speed * 0.5111) * (target_speed * 0.5111); //speed to m/s
+                    let target_potential: f64 = (target_altitude * 0.304) * 9.81; // altitude to m
+
+                    let target_energy: f64 = target_kinetic + target_potential;
+
+                    let vind: f64 = app_state.plane_state.get("Vind").unwrap().as_f64().unwrap();
+                    let altitude: f64 = app_state
+                        .plane_state
+                        .get("altitude_msl")
+                        .unwrap()
+                        .as_f64()
+                        .unwrap();
+
+                    let kinetic: f64 = 0.5 * (vind * 0.51111) * (vind * 0.51111);
+                    let potential: f64 = (altitude * 0.304) * 9.81;
+                    let energy: f64 = kinetic + potential;
+
+                    let energy_error: f64 = target_energy - energy;
+
+                    let ke: f64 = 0.0000011;
+                    let ks = 0.00000002;
+                    let thr_cruise = 0.50 + target_energy * ks;
+
+                    let throttle = (ke * energy_error + thr_cruise);
+
+                    println!(
+                        "TEC mode - alitude [ft]: {:.4}, Vind [kt]: {:.4}, energy_error: {:.4}, throttle: {:.4}",
+                        altitude, vind, energy_error, throttle
+                    );
+
+                    let mut map: HashMap<String, Value> = HashMap::new();
+                    map.insert("command".to_string(), Value::String("throttle".to_string()));
+                    map.insert(
+                        "value".to_string(),
+                        Value::Number(Number::from_f64(throttle).unwrap()),
+                    );
+
+                    let client = reqwest::Client::new();
+
+                    let _res = match client
+                        .post("http://localhost:3100/api/v1/command")
+                        .json(&map)
+                        .send()
+                        .await
+                    {
+                        Ok(_res) => {}
+                        Err(_) => {}
+                    };
+
+                    let pitch: f64 = app_state
+                        .plane_state
+                        .get("pitch")
+                        .unwrap()
+                        .as_f64()
+                        .unwrap();
+
+                    let kpitch: f64 = -1.2;
+
+                    let target_pitch: f64 = ((target_speed - vind) * kpitch).clamp(-15.0, 15.0);
+                    let pitch_error = target_pitch - pitch;
+
+                    let pitch_rate = app_state.plane_state.get("Q").unwrap().as_f64().unwrap();
+
+                    let kpr = 0.1;
+
+                    let target_pitch_rate = (pitch_error * kpr).clamp(-3.0, 3.0);
+                    let pitch_rate_error = (target_pitch_rate - pitch_rate);
+
+                    let kelevator = 0.03;
+                    let kdelevator = 0.04;
+
+                    let elevator =
+                        (kelevator * pitch_error + kdelevator * pitch_rate_error).clamp(-0.3, 0.3);
+
+                    println!(
+                        "TEC mode - pitch [deg]: {:.4}, target_pitch [deg]: {:.4}, pitch_error [deg]: {:.4}, pitch_rate: {:.4}, target_pitch_rate: {:.4}, pitch_rate_error: {:.4}, elevator {:.4}",
+                        pitch,target_pitch, pitch_error,pitch_rate, target_pitch_rate, pitch_rate_error, elevator
+                    );
+
+                    let mut map: HashMap<String, Value> = HashMap::new();
+                    map.insert("command".to_string(), Value::String("elevator".to_string()));
+                    map.insert(
+                        "value".to_string(),
+                        Value::Number(Number::from_f64(elevator).unwrap()),
+                    );
+
+                    let client = reqwest::Client::new();
+
+                    let _res = match client
+                        .post("http://localhost:3100/api/v1/command")
+                        .json(&map)
+                        .send()
+                        .await
+                    {
+                        Ok(_res) => {}
+                        Err(_) => {}
+                    };
+                }
+            }
+
             const MAX_AILERON: f64 = 0.3;
             //horizontal mode
 
@@ -126,7 +240,8 @@ async fn run_autopilot() -> anyhow::Result<()> {
                     let p: f64 = 0.01;
                     let d: f64 = 0.01;
 
-                    let aileron: f64 = (roll_error * p + roll_rate_error * d).clamp(-0.2, 0.2);
+                    let aileron: f64 =
+                        (roll_error * p + roll_rate_error * d).clamp(-MAX_AILERON, MAX_AILERON);
 
                     println!(
                         "Heading mode - heading [deg]: {:.4}, heading error [deg]: {:.4}, target_roll_angle [deg]: {:.4}, roll [deg]: {:.4}, roll_error: {:.4}, target roll rate [deg]: {:.4}, roll rate [deg/s]: {:.4}, roll_rate_error: {:.4}, aileron [0-1]: {:.4}",
@@ -156,8 +271,8 @@ async fn run_autopilot() -> anyhow::Result<()> {
                     let roll: f64 = app_state.plane_state.get("roll").unwrap().as_f64().unwrap();
                     let roll_rate = app_state.plane_state.get("P").unwrap().as_f64().unwrap();
 
-                    let p: f64 = 0.015;
-                    let d: f64 = 0.02;
+                    let p: f64 = 0.01;
+                    let d: f64 = 0.01;
 
                     let aileron: f64 =
                         (-(roll * p + roll_rate * d)).clamp(-MAX_AILERON, MAX_AILERON);
